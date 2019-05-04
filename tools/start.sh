606dc9d320fb22c5d75bc3e4bf1861cdb2d0a2f5
xxl <xxl@local.com>
2019-05-04 16:54:34 +0800

    debug

--- tools/start.sh
+++ tools/start.sh
@@ -2,6 +2,8 @@
 
 echo "start chain start ...."
 
+cd ../chain
+
 geth --datadir data --allow-insecure-unlock --networkid 1201 --rpc --rpcaddr "0.0.0.0" --rpcport "8545" --rpcapi eth,net,web3,personal,admin,miner --minerthreads 1 --rpccorsdomain "*" --maxpeers '100' --mine
 
 echo "start chain end ..."
